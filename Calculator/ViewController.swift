e9967b02834ed51d84ab54d4f2ffebf827891331
宋泽烽 <songzefeng@songzefengdeMacBook-Pro.local>
2017-03-30 10:06:33 +0800

    commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 宋泽烽 on 17/3/29.
+//  Copyright © 2017年 Zefeng Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInMiddleOfTyping = false
+    
+    @IBAction private func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInMiddleOfTyping{
+        let textCurrentInDisplay = display.text!
+        display.text = textCurrentInDisplay + digit
+        }else{
+            display.text = digit
+        }
+        userIsInMiddleOfTyping = true
+    }
+    
+    private var displayValue: Double{
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+   
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(_ sender: UIButton) {
+        if userIsInMiddleOfTyping{
+            brain.setOperand(operand: displayValue)
+            userIsInMiddleOfTyping = false
+        }
+        if let mathmaticalSymbol = sender.currentTitle{
+            brain.performOperation(symbol: mathmaticalSymbol)
+        }
+        displayValue = brain.result
+    }
+
+}
+
